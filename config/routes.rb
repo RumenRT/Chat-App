6319da24db7452a8e35b8ebe0788126ddd475bec
RumenRT <33309rt@gmail.com>
2018-10-24 17:22:20 -0400

    added actionCable and activeJob

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
-  root 'chats#index'
   mount ActionCable.server => '/cable'
+  root 'chats#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
